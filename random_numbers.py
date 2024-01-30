4711f1cda4fa903297f01e1ecca73e041f866e59
Daniel Toledo <daniel.toledo.fuentessud@gmail.com>
2024-01-30 15:21:06 -0300

    random numbers 1

---
+++ random_numbers.py
@@ -0,0 +1,10 @@
+def main():
+    numbers:[16.2,75.1,52.3]
+    print(numbers)
+    append_random_numbers()
+    print(numbers)
+    append_random_numbers()
+    print(numbers)
+
+def append_random_numbers(nunbers_list,quantity):
+    quantity 
